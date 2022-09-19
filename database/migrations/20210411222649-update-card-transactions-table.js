4da3f8a4964c24469bd0fe53ce172f5e25481299
Onuchukwu Adebayo <adebayop.o@gmail.com>
2022-09-19 01:40:05 +0100

    feat: setup jawdb instead of cleardb on heroku

--- database/migrations/20210411222649-update-card-transactions-table.js
+++ database/migrations/20210411222649-update-card-transactions-table.js
@@ -7,10 +7,6 @@ module.exports = {
       autoIncrement: true,
       type: Sequelize.INTEGER,
     });
-    await queryInterface.addColumn("card_transactions", "amount", {
-      allowNull: false,
-      type: Sequelize.DECIMAL(20, 4),
-    });
   },
 
   down: async (queryInterface, Sequelize) => {
